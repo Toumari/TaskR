63a9b3590fecae73c1d00ac101c06d7d2a035420
jackwilliams <jack.w141@outlook.com>
2018-08-05 18:00:34 +0100

    Add new item functionality complete

--- TaskR/ToDoListViewController.swift
+++ TaskR/ToDoListViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class ToDoListViewController: UITableViewController {
     
-    let itemArray = ["Make Awesome Lists","Be the best","Write Swift"]
+    var itemArray = ["Make Awesome Lists","Be the best","Write Swift"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -43,6 +43,37 @@ class ToDoListViewController: UITableViewController {
         tableView.deselectRow(at: indexPath, animated: true)
         
     }
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+    
+        
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title: "Add New TaskR Item", message: "", preferredStyle: .alert)
+    
+        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+                //What happens once user clicks add item
+            
+            self.itemArray.append(textField.text!)
+            
+            self.tableView.reloadData()
+            
+            
+        }
+        
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Create new item"
+            textField = alertTextField
+        }
+        
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
+        
+        
+    }
+    
+    
 
 }
 
